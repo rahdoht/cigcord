2a75f5a36bda8ea66cd21b7596ac80ddafc793fc
rahdoht <rahdoht@tutanota.com>
2023-09-26 16:45:50 -0500

    move interface to Commands

--- commands/hello.ts
+++ commands/hello.ts
@@ -1,12 +1,12 @@
 import { CommandInteraction, Client } from "discord.js";
-import { Command } from "../Command";
+import { Command } from "../Commands";
 
 export const Hello: Command = {
   name: "hello",
   description: "Returns a greeting",
   type: 1, // "CHAT_INPUT",
   run: async (client: Client, interaction: CommandInteraction) => {
-    const content = "Hello there!";
+    const content = "hello world";
 
     await interaction.followUp({
       ephemeral: true,
