dab8f421163371e55dffe8277803efce6b8eb25a
rahdoht <rahdoht@tutanota.com>
2023-09-26 22:00:51 -0500

    capture text from slash command

--- commands/hello.ts
+++ commands/hello.ts
@@ -1,12 +1,25 @@
-import { CommandInteraction, Client } from "discord.js";
+import {
+  CommandInteraction,
+  Client,
+  ApplicationCommandOptionType,
+} from "discord.js";
 import { Command } from "../Commands";
 
 export const Hello: Command = {
   name: "hello",
   description: "Returns a greeting",
   type: 1, // "CHAT_INPUT",
+  options: [
+    {
+      name: "text",
+      description: "Text to include in the greeting",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+  ],
   run: async (client: Client, interaction: CommandInteraction) => {
-    const content = "hello world";
+    const text = interaction.options.get("text")?.value;
+    const content = `hello world: ${text}`;
 
     await interaction.followUp({
       ephemeral: true,
