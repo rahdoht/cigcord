fcc49aa72af58be4286ecf2f92094f4dc7949e5f
rahdoht <rahdoht@tutanota.com>
2023-09-26 21:59:45 -0500

    send image

--- Commands.ts
+++ Commands.ts
@@ -5,9 +5,10 @@ import {
 } from "discord.js";
 
 import { Hello } from "./commands/hello";
+import { Image } from "./commands/image";
 
 export interface Command extends ChatInputApplicationCommandData {
   run: (client: Client, interaction: CommandInteraction) => void;
 }
 
-export const Commands: Command[] = [Hello];
+export const Commands: Command[] = [Hello, Image];
