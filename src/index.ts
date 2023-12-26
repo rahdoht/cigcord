7b77080e1b5da13d385233ef9f9c1d16444931f1
rahdoht <rahdoht@tutanota.com>
2023-12-26 14:52:58 -0600

    come back to basics

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
-import { startBot } from "./bot.ts";
+// import { startBot } from "./bot.ts";
 
-// Create an Express server to listen on the specified port
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,11 +10,11 @@ app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
-  startBot()
-    .then(() => {
-      console.log("Bot is running!");
-    })
-    .catch((error) => {
-      console.error("Error starting the bot:", error);
-    });
+  // startBot()
+  //   .then(() => {
+  //     console.log("Bot is running!");
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error starting the bot:", error);
+  //   });
 });
